2751597c5c98c2384f0e76821a56d3e11369a888
Kai Bepperling <kai.bepperling@gmail.com>
2015-05-04 16:39:42 +0200

    added source support

--- gitlabbackup.sh
+++ gitlabbackup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#For omnibus package:
+### For omnibus package ###
 #Make a new backup
 gitlab-rake gitlab:backup:create
 #Delete backups which are older than 30 days
@@ -8,3 +8,13 @@ find /home/git/gitlab/tmp/backups/*.tar -mtime +30 -exec rm {} \;
 #You need to setup s3cmd with s3cmd --configure first
 s3cmd sync --skip-existing --delete-removed /home/git/gitlab/tmp/backups/ s3://[your_bucket]/gitlab/backups/
 
+
+### For Source install ### 
+#Make a new backup
+# cd /home/git/gitlab
+# sudo -u git -H bundle exec rake gitlab:backup:create RAILS_ENV=production
+#Delete backups which are older than 30 days
+# find /home/git/gitlab/tmp/backups/*.tar -mtime +30 -exec rm {} \;
+#Sync with your S3 Server
+#You need to setup s3cmd with s3cmd --configure first
+# s3cmd sync --skip-existing --delete-removed /home/git/gitlab/tmp/backups/ s3://[your_bucket]/gitlab/backups/
